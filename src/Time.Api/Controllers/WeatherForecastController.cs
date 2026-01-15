f5e7eda1c4f8e0d0ae0defa1b2ce372b90b03c5f
DVV_COMPUTER\biker <dvv153m@gmail.com>
2026-01-16 00:52:22 +0300

    ++

--- src/Time.Api/Controllers/WeatherForecastController.cs
+++ src/Time.Api/Controllers/WeatherForecastController.cs
@@ -3,14 +3,9 @@ using Microsoft.AspNetCore.Mvc;
 namespace Time.Api.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("current-time")]
 public class WeatherForecastController : ControllerBase
-{
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
+{    
     private readonly ILogger<WeatherForecastController> _logger;
 
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
@@ -19,14 +14,8 @@ public class WeatherForecastController : ControllerBase
     }
 
     [HttpGet]
-    public IEnumerable<WeatherForecast> Get()
+    public string Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        return DateTime.Now.ToString();
     }
 }
